ccbae2023971ceef77df694cd493a0c330cb3bb9
Jason500 <12432005@mail.sustech.edu.cn>
2026-06-01 01:15:51 +0800

    initialization of project

---
+++ libs/DFV.py
@@ -0,0 +1,105 @@
+from sage.all import *
+import copy
+from functions import *
+
+
+class DFV:
+    id = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
+    
+    def __init__(self,D,F,V,is_interior=False,is_canonical=False):
+        """
+        class DFV: for managing Delaunay with faces and dual(Voronoi).
+        D: Delaunay graph;
+        F: Faces of Delaunay;
+        V: Voronoi dual of D.
+        """
+        self.D = D
+        self.F = F
+        self.V = V
+        self.card_E = len(D.edges())
+        self.card_F = len(F)
+        self._poly = None
+        self._dim = None
+        self._faces = None
+
+        self.is_interior = None
+        if is_interior: 
+            self.is_interior = True
+
+        self.is_canonical = None
+        self.canonical_image = None
+        if is_canonical: 
+            self.is_canonical = True
+            self.canonical_image = (self,DFV.id)
+
+        # self.edge_dict = *under construction*
+        
+        
+    def remove_single_edge(self,i):
+        """
+        Return the subgraphs obtained by removing ith edge from given Delaunay graph D, with modification of F and V.
+        """
+        d = self.D.copy()
+        ei = d.edges()[i]
+        d.delete_edge(ei)
+        f,v = modified_faces_with_voronoi(ei,self.F,self.V)
+        return DFV(d,f,v)
+
+    def remove_multiple_edges(self,indices,is_interior=True):
+        d, f, v = [copy.deepcopy(x) for x in (self.D, self.F, self.V)]
+        e_list = self.D.edges()
+        for i in indices:
+            d.delete_edge(e_list[i])
+            f,v = modified_faces_with_voronoi(e_list[i],f,v)
+        return DFV(d,f,v,is_interior)
+    
+    @property
+    def poly(self):
+        if self._poly: return self._poly
+        self._poly = poly(self.D,self.F,self.V)
+        self._dim = self.poly.dim()
+        return self._poly
+    
+    @property
+    def dim(self):
+        return self.poly.dim()
+
+    @property
+    def faces(self):
+        """
+        Return the faces: [interiors, boundaries]
+        """
+        if self._faces: return self._faces
+        raw_interior_faces = []
+        raw_boundary_faces = []
+        voronoi_edge_dict = {label:(start,end) for start,end,label in self.V.edges()}
+        for face in self.poly.facets():
+            center = face.as_polyhedron().center()
+            index_of_collapsed_edges = [i for i, x in enumerate(center) if x == 1]
+            labels_of_collapsed_edges = [self.D.edges()[i][2] for i in index_of_collapsed_edges]
+
+            if 0 in center: # non-separating degeneration
+                raw_boundary_faces.append(self.remove_multiple_edges(index_of_collapsed_edges,is_interior=False))
+
+            elif not Graph([voronoi_edge_dict[label] for label in labels_of_collapsed_edges],multiedges=True, loops=True).is_forest(): 
+                # separating degeneration. I think it will always occur by pinching a single selfloop in V. But first we still detect the loop.
+                raw_boundary_faces.append(self.remove_multiple_edges(index_of_collapsed_edges,is_interior=False))
+
+            else:
+                raw_interior_faces.append(self.remove_multiple_edges(index_of_collapsed_edges,is_interior=True))
+        self._faces=[raw_interior_faces,raw_boundary_faces]
+        return self._faces
+        
+    def return_DFV_tuple(self):
+        return (self.D,self.F,self.V)
+
+    def is_orientation_preserving_isomorphic_to(self, dfv):
+        return is_orientation_preserving_isomorphic(self, dfv)
+
+    def set_canonical_image(self,dfv,iso):
+        self.is_canonical = False
+        self.canonical_image = (dfv,iso)
+
+    def set_canonical(self):
+        self.is_canonical = True
+        self.canonical_image = (self,DFV.id)
---
+++ libs/functions.py
@@ -0,0 +1,208 @@
+from sage.all import *
+from sage.geometry.polyhedron.constructor import Polyhedron
+
+def is_edge_on_face(edge,face):
+    """
+    Determine if an edge is on a given face.
+
+    Modified from edge_on_faces()
+    """
+    return any([e[2]==edge[2] for e in face])
+
+def is_edge_on_faces(edge,faces):
+    """
+    Determine if an edge is contained by a bunch of face.
+
+    Modified from edge_on_faces()
+    """
+    return any([is_edge_on_face(edge,f) for f in faces])
+
+def is_loop(edge):
+    """
+    Determine if an edge is a loop.
+    """
+    return edge[0]==edge[1]
+
+def generate_voronoi(D,F):
+    """
+    Generate Voronoi graph (with label) with Delaunay and corresponding faces. the number representing vertex is the corresponding index of face.
+
+        D: Delaunay graph;
+        F: Faces of Delaunay;
+
+    Modified from generate_voronoi()
+    """
+    V = []
+    for e in D.edges():
+        indices = [i for i in range(len(F)) if is_edge_on_face(e,F[i])]
+        if len(indices)==1:
+            V.append((indices[0],indices[0],e[2]))
+        else:
+            V.append((indices[0],indices[1],e[2]))
+    
+    return Graph(V, multiedges=True, loops=True)
+
+
+def poly(D,F):
+    """
+    Calculate the dimension of polygon representing the cell labelled by given Delaunay graph with face information.
+
+        D: Delaunay graph;
+        
+        F: Faces of Delaunay;
+
+    Modified from poly_dimension()
+    """
+
+    n = len(D.edges()) # amount of parameters
+
+    ieq_constraints = []
+    eq_constraints = []
+
+    # constraints that all \theta_e are between 0 and 1 for any edge e. (scaled from [0,\pi]) 24 constraints.
+    for i in range(n):
+        lb = [1 if j==i+1 else 0 for j in range(n+1)] # lower bound
+        ub = [1 if j==0 else -1 if j==i+1 else 0 for j in range(n+1)] # upper bound
+        ieq_constraints += [tuple(lb),tuple(ub)]
+
+    # constraints that came from Rivin's Theorem: -|F'|+\sum \theta_e' >=0. 255 constraints.
+    psf = list(powerset(range(len(F))))
+    for s in psf: # generate constraints for all subset; Idea 1 applys here.
+        if s:
+            sf = [F[si] for si in s]
+            tc = [-len(s)] + [1 if is_edge_on_faces(e,sf) else 0 for e in D.edges()] 
+            ieq_constraints += [tuple(tc)]
+
+    # constraints that cone angles at vertices of Delaunay are all pi. equations. 6 constraints.
+    for v in D.vertices():
+        vc = [D.degree(v)-1] + [(-2 if is_loop(e) else -1) if v in e else 0 for e in D.edges()]
+        eq_constraints += [tuple(vc)]
+    
+    return Polyhedron(ieqs=ieq_constraints,eqns=eq_constraints,base_ring=QQ)
+
+
+# methods for modification
+def edge_removed_face(edge,face):
+    """
+    Return face removed given edge. If no such edge exist, return original face.
+
+    Modified from face_modification()
+    """
+    if not is_edge_on_face(edge,face):
+        return face
+    else:
+        return [e for e in face if e[2]!=edge[2]]
+
+def merge_faces(edge,faces):
+    """
+    Merge two faces along single edge.
+
+    Modified from face_modification()
+    """
+    if len(faces)>2 or not faces:
+        return ('error')
+    elif len(faces)==1:
+        return edge_removed_face(edge,faces[0]) # This case will generate disconnected graph, which will be ruled out.
+    else:
+        face1, face2 = tuple(faces)
+        face = []
+        for i, e in enumerate(face1):
+            if e[2] == edge[2]:
+                face += face1[i+1:] + face1[:i]
+                break
+        for i, e in enumerate(face2):
+            if e[2] == edge[2]:
+                face += face2[i+1:] + face2[:i]
+                break
+        return face
+        
+
+def modified_faces_with_voronoi(edge,F,V):
+    """
+    Return faces removed given edge and the corresponding Voronoi graph. The faces separated by this edge will be fused together. 
+
+    Note: the edge here is from Delaunay graph, only useful information is its label.
+
+    Modified from face_modification()
+    """
+    for e in V.edges():
+        if e[2]==edge[2]: 
+            fusing_vertices = set(e[:2])
+            v=V.copy()
+            v.delete_edge(e)
+            break
+    
+    nf = len(F)
+    faces_to_merge = [F[vi] for vi in fusing_vertices]
+    f = [F[i] for i in range(nf) if i not in fusing_vertices]
+    f.append(merge_faces(edge,faces_to_merge)) # Adjust the order of edges from fused face. This fused face is at len(F)-len(f..._v...) position.
+
+    # construct mapping of vertices for contracting Voronoi.
+    mapping_v = [
+        nf - len(fusing_vertices) if i in fusing_vertices
+        else i - sum(1 for j in fusing_vertices if j < i)
+        for i in range(nf)
+    ]
+
+    v = Graph([(mapping_v[e[0]],mapping_v[e[1]],e[2]) for e in v.edges()], multiedges=True, loops=True)
+    return (f,v)
+
+# methods for determining if isomorphic
+def canonical_face(face):
+    n = len(face)
+    face_nolabel = [(e[0],e[1]) for e in face]
+    rotations = [tuple(face_nolabel[i:] + face_nolabel[:i]) for i in range(n)]
+    # reversed_rotations = [tuple((v2, v1) for (v1,v2,_) in reversed(r)) for r in rotations]
+    # print(reversed_rotations)
+    # return min(rotations + reversed_rotations)
+    return min(rotations)
+
+def flipped_canonical_face(face):
+    n = len(face)
+    face_nolabel_flipped = [(e[1],e[0]) for e in face]
+    rotations = [tuple(face_nolabel_flipped[i:] + face_nolabel_flipped[:i]) for i in range(n)]
+    return min(rotations)
+
+def canonical_image(face,map):
+    image = canonical_face([
+        (map(u),map(v))
+        for (u,v,_) in face
+    ])
+    return image
+
+def is_orientation_preserving_isomorphic(DFV1, DFV2):
+    if not DFV1.V.is_isomorphic(DFV2.V):
+        return False, None
+    
+    # 检查 Delaunay 图是否同构
+    is_iso, iso_map = DFV1.D.is_isomorphic(DFV2.D, certificate=True)
+
+    # 一个特判: 把0维退化胞腔丢掉.
+    if DFV1.poly.is_empty():
+        return True, iso_map
+
+    if not is_iso:
+        # print("not iso") 
+        return False, None
+    else:
+        canonical_f2 = [canonical_face(f2) for f2 in DFV2.F]
+        
+        for phi in DFV1.D.automorphism_group():
+            # 构造顶点置换dict
+            current_map_dict = {v: iso_map[phi(v)] for v in DFV1.D.vertices()}
+            
+            mapping_func = lambda a: current_map_dict[a]
+            
+            is_iso = True
+            for f1 in DFV1.F: # 检查所有面是否保持定向
+                if all(f2 != canonical_image(f1, mapping_func) for f2 in canonical_f2):
+                    is_iso = False
+                    break
+            
+            # 如果找到了保持定向的同构，返回 True 和该顶点映射字典
+            if is_iso: 
+                return True, current_map_dict
+        
+        return False, None
+
+    
